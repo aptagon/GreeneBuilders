2e8ff1498f7d7b43034e19bdf6d4fe29bfda456a
massivesyed3 <massivesyed3@gmail.com>
2024-05-10 16:53:40 +0500

    Changes

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,10 +6,8 @@ export default function Footer () {
             <footer className="text-center text-lg-start footer">
                 <section className="d-flex pt-4 pb-4 pl-0 border-bottom container">
                     <div className="d-flex justify-content-left">
-                        <a href="/" className="me-3"><FontAwesomeIcon icon={faFacebook} size="xl" /></a>
-                        <a href="/" className="me-3 text-reset"><FontAwesomeIcon icon={faInstagram} size="xl" /></a>
-                        <a href="/" className="me-3 text-reset"><FontAwesomeIcon icon={faTwitter} size="xl" /></a>
-                        <a href="/" className="text-reset"><FontAwesomeIcon icon={faLinkedin} size="xl" /></a>
+                        <a href="https://www.facebook.com/profile.php?id=100052653267274" className="me-3"><FontAwesomeIcon icon={faFacebook} size="xl" /></a>
+                        <a href="https://www.linkedin.com/in/david-greene-56b2a42a/" className="text-reset"><FontAwesomeIcon icon={faLinkedin} size="xl" /></a>
                     </div>
                     <div className="me-5 d-lg-block copy-right">
                         <span>Copyright © 2024 Greene Builders, Inc. All Rights Reserved</span>
